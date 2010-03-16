5eed014441151369d72a3e758edc8419abd0e456
Mikl Kurkov <mkurkov@gmail.com>
2010-03-16 18:32:32 +0300

    Add remember support

--- mk-remember.el
+++ mk-remember.el
@@ -20,4 +20,25 @@
                                      ("note" ?n "* %?                                        :NOTE:\n  :CLOCK:\n  :END:\n  %U\n  %a" nil bottom nil)
                                      )))
 
+;;; function to run remember outside emacs
+(defadvice remember-finalize (after delete-remember-frame activate)
+  "Advise remember-finalize to close the frame if it is the remember frame"
+  (if (equal "remember" (frame-parameter nil 'name))
+      (delete-frame)))
 
+(defadvice remember-destroy (after delete-remember-frame activate)
+  "Advise remember-destroy to close the frame if it is the rememeber frame"
+  (if (equal "remember" (frame-parameter nil 'name))
+      (delete-frame)))
+
+;; make the frame contain a single window. by default org-remember
+;; splits the window.
+(add-hook 'remember-mode-hook
+          'delete-other-windows)
+
+(defun make-remember-frame ()
+  "Create a new frame and run org-remember."
+  (interactive)
+  (make-frame '((name . "remember") (width . 80) (height . 10)))
+  (select-frame-by-name "remember")
+  (org-remember))
