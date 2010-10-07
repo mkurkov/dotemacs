5b1100cc1f8cb2b30331f431b8ea5eaf602531d4
Mikl Kurkov <mkurkov@gmail.com>
2010-10-07 14:54:44 +0400

    Fix path to i18n.yml for cucumber mode

--- mk-cucumber.el
+++ mk-cucumber.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/emacs/lib/cucumber")
+(setq feature-default-i18n-file "~/emacs/lib/cucumber/i18n.yml")
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
