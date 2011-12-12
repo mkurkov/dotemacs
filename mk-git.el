3d5912367834e976809888f51cc17bf5bf6e1e67
Mikl Kurkov <mkurkov@gmail.com>
2011-12-12 18:08:05 +0400

    Move color theme to el-get from gitsubmodules

--- mk-git.el
+++ mk-git.el
@@ -1,12 +1,13 @@
-(add-to-list 'load-path "~/emacs/lib/egg")
-(require 'egg)
+;;(add-to-list 'load-path "~/emacs/lib/egg")
+;;(require 'egg)
 
-(add-to-list 'load-path "~/emacs/lib/magit")
-(require 'magit)
 
-(add-to-list 'load-path "~/emacs/lib/mo-git-blame")
 
-(autoload 'mo-git-blame-file "mo-git-blame" nil t)
+;;(add-to-list 'load-path "~/emacs/lib/magit")
+;;(require 'magit)
+
+;;(add-to-list 'load-path "~/emacs/lib/mo-git-blame")
+
+;;(autoload 'mo-git-blame-file "mo-git-blame" nil t)
+;;
 (autoload 'mo-git-blame-current "mo-git-blame" nil t)
-
-
