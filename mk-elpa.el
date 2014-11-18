96caedbc127c854b21b081e86da493a8ba4a5b89
Mikl Kurkov <mkurkov@gmail.com>
2014-11-18 18:32:50 +0300

    Fix elpa setup - refresh contents on first start

--- mk-elpa.el
+++ mk-elpa.el
@@ -1,7 +1,12 @@
+(require 'package)
+
 (setq package-archives '(
    ("ELPA" . "http://tromey.com/elpa/")
    ("gnu" . "http://elpa.gnu.org/packages/")
    ("marmalade" . "http://marmalade-repo.org/packages/")
    ("melpa" . "http://melpa.milkbox.net/packages/")
 ))
+
 (package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
